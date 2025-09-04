a2f11ccf810a03ddaa397f3bd03237da8e4c8dc5
GOGOKETSU <319shidou@gmail.com>
2025-09-04 13:02:12 +0900

    sitemap微調整

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://hotospot.com", // 自分のドメイン
+  siteUrl: "https://www.hotospot.com", // 自分のドメイン
   generateRobotsTxt: true,        // robots.txt も生成
 };
